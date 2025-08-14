b89982766ec819ee1af3e241e20559df5d3ef6c7
Sebastian Arboleda <juansebastian_gomez456@hotmail.com>
2025-08-14 00:02:45 -0500

    Se encarpeta producto.html y se elimina genero de formulario

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -4,7 +4,7 @@ const productos = [
         id: 1,
         nombre: "Camiseta manga corta tradicional",
         precio: 149900,
-        imagen: "assets/media/images/producto1.webp", // ruta relativa
+        imagen: "/assets/media/images/producto1.webp", // ruta relativa
         descripcion: "Camiseta clásica con corte recto y cuello redondo.",
         colores: ["#cfcfcf", "#000000", "#ffffff"],
         tallas: ["S", "M", "L", "XL"]
@@ -13,7 +13,7 @@ const productos = [
         id: 2,
         nombre: "Camisa manga larga rayas",
         precio: 199900,
-        imagen: "assets/media/images/producto2.webp", // ruta relativa
+        imagen: "/assets/media/images/producto2.webp", // ruta relativa
         descripcion: "Camisa con diseño a rayas, ideal para ocasiones formales.",
         colores: ["#f0f0f0", "#1a1a1a"],
         tallas: ["S", "M", "L"]
